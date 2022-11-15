ca186fc27c4cd094cfa6c34a99f53d64a89129aa
hugo-pb <h.palomerabecerra@gmail.com>
2022-11-15 13:34:59 -0700

    get all articles

--- main.js
+++ main.js
@@ -4,9 +4,27 @@ const axios = require("axios");
 
 const topArticles = (limit) => {
   // make a request to Api/multiple request to get all pages
-  axios
-    .get("https://jsonmock.hackerrank.com/api/articles?page=1")
-    .then((res) => console.log(res.data));
+  async function downloadAllArticles() {
+    let articles = [];
+    let page = 0;
+    let totalPages = 0;
+
+    do {
+      let { data: response } = await axios.get(
+        `https://jsonmock.hackerrank.com/api/articles?page=${page++}`
+      );
+      totalPages = response.total_pages;
+      console.log(
+        `downloadarticles: page ${page} of ${totalPages} downloaded...`
+      );
+      articles = articles.concat(response.data);
+      console.log("articles.length:", articles.length);
+    } while (page < totalPages);
+
+    console.log("downloadarticles: download complete.");
+
+    return articles;
+  }
 
   // query response in comment dec (most comments on top) if there is a tie
   // query aph order
@@ -16,5 +34,7 @@ const topArticles = (limit) => {
   //     Otherwise, if the story_title field is not null, use story_title.
   //     If both fields are null, ignore the article.
   // Sort the titles decreasing by comment count, then decreasing alphabetically by article name if there is a tie in comments count. Return a list of the top limit names.
+
+  downloadAllArticles().then((data) => console.log(data));
 };
 topArticles();
